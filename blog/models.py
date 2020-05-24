9668ecb110804f2aa7484a5046e036da5b55e735
SagarRathi <blastofftopluto@gmail.com>
2020-05-24 22:30:44 +0530

    added code support v1

--- blog/models.py
+++ blog/models.py
@@ -41,11 +41,6 @@ from wagtail.search.models import Query
 ####################### Imports Ends Here ###############
 
 
-class BlogIndexPage(RoutablePageMixin,Page):
-    intro=RichTextField(blank=True)
-    content_panels=Page.content_panels+[
-        FieldPanel('intro', classname='full')
-    ]
 
     def get_context(self, request,*args, **kwargs):
         context=super().get_context(request,*args, **kwargs)
