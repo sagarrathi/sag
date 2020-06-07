1213b1da049b82f1af7aa6e57bd290c1cac1b21c
SagarRathi <blastofftopluto@gmail.com>
2020-06-07 19:19:17 +0530

    react parent js

--- xui/src/App.js
+++ xui/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+import Header from "./components/Header.js"
+
 
 function App(){
   return (
   <div>
     <Header />
-    <MainContent />
-    <Footer />
-
   </div>
   )
 } 
--- xui/src/components/Header.js
+++ xui/src/components/Header.js
@@ -2,9 +2,7 @@ import React from "react"
 
 function Header(){
     return(
-        <Header>
           <h1>This is header</h1>  
-        </Header>
     )
 }
 
